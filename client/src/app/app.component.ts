819e41ec1fc3995a01e47587e891adf526c98ad9
Hayden Anderson <102517842+haydenbruceanderson12@users.noreply.github.com>
2024-11-25 11:35:09 +0200

    test git config

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test git config client';
+  title = 'test git config';
 }
